6bce145b51f81a3246cc810cd6fd8ca6dbfbfed9
kitlith <kitlith@kitl.pw>
2023-02-24 00:59:43 +0000

    Deploying to docs from @ kitlith/SlimeVR-Rust@794b3be38938e32ecbebd2675b7e4499da670803 🚀

--- crates.js
+++ crates.js
@@ -1,1 +1,1 @@
-window.ALL_CRATES = ["autoupdater","firmware_protocol","skeletal_model","slimevr_overlay","solarxr","tokio_shutdown","vqf"];
+window.ALL_CRATES = ["autoupdater","firmware_protocol","skeletal_model","skeletal_model_napi","slimevr_overlay","solarxr","tokio_shutdown","vqf"];
